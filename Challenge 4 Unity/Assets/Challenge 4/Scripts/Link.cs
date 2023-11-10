0a4bc054fab6413d8943d883fa044fdd3a481a5e
janouskie <b00149565@mytudublin.ie>
2023-11-10 20:36:44 +0000

    Added button and love

---
+++ Challenge 4 Unity/Assets/Challenge 4/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenGithub()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/janouskie/Challenge-4-Submission2");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
